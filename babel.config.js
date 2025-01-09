833448904a847cdb6a26c668dde2c2309854de3b
muratdayan <muratdayan359@gmail.com>
2025-01-09 14:10:30 +0300

    Initial Commit witd adding dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    'react-native-reanimated/plugin', // Diğer pluginler bu dizide olmalı
+  ],
 };
